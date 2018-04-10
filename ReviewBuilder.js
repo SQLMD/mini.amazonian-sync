11bee9ee5f39f556269ef648ec21031028136f7d
Mike Donnelly <sqldonnelly@gmail.com>
2018-04-10 23:30:59 +0900

    Completed ReviewBuilder

--- ReviewBuilder.js
+++ ReviewBuilder.js
@@ -1,7 +1,24 @@
 const fs = require("fs");
 
 class ReviewBuilder {
-  buildReviewsSync() {}
+  buildReviewsSync() {
+    const reviews = JSON.parse(fs.readFileSync("./data/reviews.json", "utf8"));
+    const products = JSON.parse(
+      fs.readFileSync("./data/products.json", "utf8")
+    );
+    const users = JSON.parse(fs.readFileSync("./data/users.json", "utf8"));
+
+    return reviews.map((review) => {
+      return {
+        productName: products.find(
+          (product) => product.id === review["productId"]
+        ).name,
+        username: users.find((user) => user.id === review["userId"]).username,
+        text: review["text"],
+        rating: review["rating"],
+      };
+    });
+  }
 }
 
 module.exports = ReviewBuilder;
